f6c1266fdb6ae1ac09cfe73866be4271e69888ec
OliviaAo <aoli.lady813@gmail.com>
2018-03-19 13:59:27 +1100

    changes

--- 3. PHI-Reducer/phi-reducer-0.4.5/phireducer/testNLP.py
+++
@@ -1,8 +0,0 @@
-import spacy 
-
-nlp = spacy.load('en')
-test = open('testcase.xml').read()
-
-doc = nlp(test)
-
-print (doc)
--- 4. MIST/1.Demo/src/tasks/core/python/deid_ws.sql
+++
@@ -1,7 +0,0 @@
-/* Extra SQL table for nomination locks. */
-
-CREATE TABLE IF NOT EXISTS nomination_lock (
-  doc_name TEXT PRIMARY KEY NOT NULL,
-  locked_by TEXT NOT NULL,
-  lock_id TEXT NOT NULL
-);
